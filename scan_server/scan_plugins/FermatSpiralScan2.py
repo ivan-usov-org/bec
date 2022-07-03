2ab6ad6274bc147cb289b294a9955142dcc871a7
Klaus Wakonig <klaus.wakonig@psi.ch>
2022-07-03 22:59:22 +0200

    added more comments

--- scan_server/scan_plugins/FermatSpiralScan2.py
+++ scan_server/scan_plugins/FermatSpiralScan2.py
@@ -1,3 +1,25 @@
+"""
+SCAN PLUGINS
+
+All new scans should be derived from ScanBase. ScanBase provides various methods that can be customized and overriden
+but they are executed in a specific order:
+
+- self.initialize                        # initialize the class if needed
+- self.read_scan_motors                  # used to retrieve the start position (and the relative position shift if needed)
+- self.prepare_positions                 # prepare the positions for the scan. The preparation is split into multiple sub fuctions:
+    - self._calculate_positions          # calculate the positions
+    - self._set_positions_offset         # apply the previously retrieved scan position shift (if needed)
+    - self._check_limits                 # tests to ensure the limits won't be reached
+- self.open_scan                         # send and open_scan message including the scan name, the number of points and the scan motor names
+- self.stage                             # stage all devices for the upcoming acquisiton
+- self.run_baseline_readings             # read all devices to get a baseline for the upcoming scan
+- self.scan_core                         # run a loop over all position 
+    - self._at_each_point(ind, pos)      # called at each position with the current index and the target positions as arguments
+- self.finalize                          # clean up the scan, e.g. move back to the start position; wait everything to finish
+- unstage                                # unstage all devices that have been staged before
+- cleanup                                # send a close scan message and perform additional cleanups if needed
+"""
+
 from scan_server.scans import ScanArgType, ScanBase, get_fermat_spiral_pos
 
 
