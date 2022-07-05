7e175c4d4e46ec4187445eb569b6f50a3396b807
Klaus Wakonig <klaus.wakonig@psi.ch>
2022-07-05 16:13:27 +0200

    added more end2end tests

--- bec_client/tests/end-2-end/test_scans.py
+++ bec_client/tests/end-2-end/test_scans.py
@@ -54,6 +54,49 @@ def test_line_scan(capsys):
     assert "finished. Scan ID" in captured.out
 
 
+def test_mv_scan(capsys):
+    bec = start_client()
+    scans = bec.scans
+    dev = bec.devicemanager.devices
+    scans.mv(dev.samx, 10, dev.samy, 20)
+    current_pos_samx = dev.samx.read()
+    current_pos_samy = dev.samy.read()
+    assert current_pos_samx["samx"]["value"] == 10
+    assert current_pos_samy["samy"]["value"] == 20
+    scans.umv(dev.samx, 10, dev.samy, 20)
+    captured = capsys.readouterr()
+    ref_out_samx = "samx:     10.00 ━━━━━━━━━━━━━━━      10.00 /      10.00 / 100 % 0:00:00 0:00:00"
+    ref_out_samy = "samy:     20.00 ━━━━━━━━━━━━━━━      20.00 /      20.00 / 100 % 0:00:00 0:00:00"
+    assert ref_out_samx in captured.out
+    assert ref_out_samy in captured.out
+
+
+def test_mv_scan_mv():
+    bec = start_client()
+    scans = bec.scans
+    dev = bec.devicemanager.devices
+    scans.mv(dev.samx, 10, dev.samy, 20)
+    current_pos_samx = dev.samx.read()
+    current_pos_samy = dev.samy.read()
+    assert current_pos_samx["samx"]["value"] == 10
+    assert current_pos_samy["samy"]["value"] == 20
+    s = scans.grid_scan(dev.samx, -5, 5, 10, dev.samy, -5, 5, 10, exp_time=0.01)
+    assert len(s.scan.data) == 100
+    assert s.scan.num_points == 100
+    current_pos_samx = dev.samx.read()
+    current_pos_samy = dev.samy.read()
+    assert current_pos_samx["samx"]["value"] == 10
+    assert current_pos_samy["samy"]["value"] == 20
+    scans.umv(dev.samx, 20, dev.samy, -20)
+    s = scans.grid_scan(dev.samx, -5, 5, 10, dev.samy, -5, 5, 10, exp_time=0.01)
+    assert len(s.scan.data) == 100
+    assert s.scan.num_points == 100
+    current_pos_samx = dev.samx.read()
+    current_pos_samy = dev.samy.read()
+    assert current_pos_samx["samx"]["value"] == 20
+    assert current_pos_samy["samy"]["value"] == -20
+
+
 def test_scan_abort():
     def send_abort():
         time.sleep(2)
