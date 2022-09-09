ae1d8d91d4df1c83143472fef6b328e01a024434
Klaus Wakonig <klaus.wakonig@psi.ch>
2022-09-09 19:26:38 +0200

    added config_handler

---
+++ device_server/device_server/devices/config_handler.py
@@ -0,0 +1,99 @@
+from bec_utils import BECMessage as BMessage
+from bec_utils import (
+    DeviceConfigError,
+    DeviceManagerBase,
+    MessageEndpoints,
+    ProducerConnector,
+    bec_logger,
+)
+
+logger = bec_logger.logger
+
+
+class ConfigHandler:
+    def __init__(self, producer: ProducerConnector, device_manager: DeviceManagerBase) -> None:
+        self.producer = producer
+        self.device_manager = device_manager
+
+    def send_config(self, msg: BMessage.DeviceConfigMessage) -> None:
+        """broadcast a new config"""
+        self.producer.send(MessageEndpoints.device_config(), msg.dumps())
+
+    def parse_config_request(self, msg: BMessage.DeviceConfigMessage) -> None:
+        """Processes a config request. If successful, it emits a config reply
+
+        Args:
+            msg (BMessage.DeviceConfigMessage): Config request
+
+        """
+        try:
+            self.device_manager._check_request_validity(msg)
+            if msg.content["action"] != "update":
+                return
+            updated = False
+            for dev in msg.content["config"]:
+                dev_config = msg.content["config"][dev]
+                device = self.device_manager.devices[dev]
+                if "deviceConfig" in dev_config:
+                    # store old config
+                    old_config = device.config["deviceConfig"].copy()
+
+                    # apply config
+                    try:
+                        self.device_manager.update_config(device.obj, dev_config["deviceConfig"])
+                    except Exception as exc:
+                        self.device_manager.update_config(device.obj, old_config)
+                        raise DeviceConfigError(f"Error during object update. {exc}") from exc
+
+                    device.config["deviceConfig"].update(dev_config["deviceConfig"])
+
+                    # update config in DB
+                    self.update_device_config_in_db(device_name=dev)
+                    updated = True
+
+                if "enabled" in dev_config:
+                    device.config["enabled"] = dev_config["enabled"]
+                    # update device enabled status in DB
+                    self.update_device_enabled_in_db(device_name=dev)
+                    updated = True
+
+            # send updates to services
+            if updated:
+                self.send_config(msg)
+
+        except DeviceConfigError as dev_conf_error:
+            self.device_manager.send_config_request_rejection(dev_conf_error)
+
+    def update_device_enabled_in_db(self, device_name: str) -> None:
+        """Update a device enabled setting in the DB with the local version
+
+        Args:
+            device_name (str): Name of the device that should be updated
+
+        Raises:
+            DeviceConfigError: Raised if the db update fails.
+        """
+        logger.debug("updating in DB")
+        success = self.device_manager._scibec.patch_device_config(
+            self.device_manager.devices[device_name].config["id"],
+            {"enabled": self.device_manager.devices[device_name].enabled},
+        )
+        if not success:
+            raise DeviceConfigError("Error during database update.")
+
+    def update_device_config_in_db(self, device_name: str) -> None:
+        """Update a device config in the DB with the local version
+
+        Args:
+            device_name (str): Name of the device that should be updated
+
+        Raises:
+            DeviceConfigError: Raised if the db update fails.
+        """
+        logger.debug("updating in DB")
+        success = self.device_manager._scibec.patch_device_config(
+            self.device_manager.devices[device_name].config["id"],
+            {"deviceConfig": self.device_manager.devices[device_name].config["deviceConfig"]},
+        )
+        if not success:
+            raise DeviceConfigError("Error during database update.")
