8505e551983b5cf0ccf076cc33601a073fa78af0
Klaus Wakonig <klaus.wakonig@psi.ch>
2022-09-09 18:48:10 +0200

    added device init test

--- bec_utils/bec_utils/tests/utils.py
+++ bec_utils/bec_utils/tests/utils.py
@@ -1,6 +1,11 @@
 from bec_utils.connector import ConnectorBase
 
 
+class PipelineMock:
+    def execute(self):
+        pass
+
+
 class ConsumerMock:
     def start(self):
         pass
@@ -9,26 +14,35 @@ class ConsumerMock:
 class ProducerMock:
     message_sent = {}
 
-    def set(self, topic, msg):
+    def set(self, topic, msg, pipe=None):
         pass
 
-    def send(self, topic, msg):
+    def send(self, topic, msg, pipe=None):
         self.message_sent = {"queue": topic, "msg": msg}
 
-    def set_and_publish(self, topic, msg):
+    def set_and_publish(self, topic, msg, pipe=None):
         self.message_sent = {"queue": topic, "msg": msg}
 
-    def lpush(self, topic, msg):
+    def lpush(self, topic, msg, pipe=None):
         pass
 
-    def rpush(self, topic, msg):
+    def rpush(self, topic, msg, pipe=None):
         pass
 
-    def lrange(self, topic, start, stop):
+    def lrange(self, topic, start, stop, pipe=None):
         return []
 
-    def get(self, topic):
+    def get(self, topic, pipe=None):
         return None
+
+    def pipeline(self):
+        return PipelineMock()
+
+    def delete(self, topic, pipe=None):
+        pass
+
+    def lset(self, topic: str, index: int, msgs: str, pipe=None) -> None:
+        pass
 
 
 class ConnectorMock(ConnectorBase):
