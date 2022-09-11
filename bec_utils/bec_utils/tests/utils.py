65c99a6dfe83d031c491203d385343927241241a
Klaus Wakonig <klaus.wakonig@psi.ch>
2022-09-11 16:24:37 +0200

    added more scan worker tests

--- bec_utils/bec_utils/tests/utils.py
+++ bec_utils/bec_utils/tests/utils.py
@@ -2,8 +2,14 @@ from bec_utils.connector import ConnectorBase
 
 
 class PipelineMock:
+    _pipe_buffer = []
+    _producer = None
+
+    def __init__(self, producer) -> None:
+        self._producer = producer
+
     def execute(self):
-        pass
+        return [getattr(self._producer, method)(*args) for method, args in self._pipe_buffer]
 
 
 class ConsumerMock:
@@ -13,36 +19,64 @@ class ConsumerMock:
 
 class ProducerMock:
     message_sent = {}
+    _get_buffer = {}
 
     def set(self, topic, msg, pipe=None):
-        pass
+        if pipe:
+            pipe._pipe_buffer.append(("set", (topic, msg)))
+            return
 
     def send(self, topic, msg, pipe=None):
+        if pipe:
+            pipe._pipe_buffer.append(("send", (topic, msg)))
+            return
         self.message_sent = {"queue": topic, "msg": msg}
 
     def set_and_publish(self, topic, msg, pipe=None):
+        if pipe:
+            pipe._pipe_buffer.append(("set_and_publish", (topic, msg)))
+            return
         self.message_sent = {"queue": topic, "msg": msg}
 
     def lpush(self, topic, msg, pipe=None):
-        pass
+        if pipe:
+            pipe._pipe_buffer.append(("lpush", (topic, msg)))
+            return
 
     def rpush(self, topic, msg, pipe=None):
+        if pipe:
+            pipe._pipe_buffer.append(("rpush", (topic, msg)))
+            return
         pass
 
     def lrange(self, topic, start, stop, pipe=None):
+        if pipe:
+            pipe._pipe_buffer.append(("lrange", (topic, msg)))
+            return
         return []
 
     def get(self, topic, pipe=None):
-        return None
+        if pipe:
+            pipe._pipe_buffer.append(("get", (topic,)))
+            return
+        val = self._get_buffer.get(topic)
+        if isinstance(val, list):
+            return val.pop(0)
+        self._get_buffer.pop(topic)
+        return val
 
     def pipeline(self):
-        return PipelineMock()
+        return PipelineMock(self)
 
     def delete(self, topic, pipe=None):
-        pass
+        if pipe:
+            pipe._pipe_buffer.append(("delete", (topic,)))
+            return
 
     def lset(self, topic: str, index: int, msgs: str, pipe=None) -> None:
-        pass
+        if pipe:
+            pipe._pipe_buffer.append(("lrange", (topic, index, msgs)))
+            return
 
 
 class ConnectorMock(ConnectorBase):
