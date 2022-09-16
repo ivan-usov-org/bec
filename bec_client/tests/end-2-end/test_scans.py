405ef35788aea91c3a48b10cf51ca357cf79d7bc
Klaus Wakonig <klaus.wakonig@psi.ch>
2022-09-16 21:11:45 +0200

    abort scan test

--- bec_client/tests/end-2-end/test_scans.py
+++ bec_client/tests/end-2-end/test_scans.py
@@ -225,8 +225,9 @@ def test_scan_abort(client):
     assert aborted_scan is True
     while bec.queue.scan_storage.storage[0].status == "open":
         time.sleep(0.5)
+    scans.line_scan(dev.samx, -5, 5, steps=50, exp_time=0.1)
     scan_number_end = bec.queue.current_scan_number
-    assert scan_number_end == scan_number_start + 1
+    assert scan_number_end == scan_number_start + 2
 
 
 @pytest.mark.timeout(100)
