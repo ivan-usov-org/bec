c20fed129027d9063865daa129e1673ec33be47a
stalbe_j <jonathan.stalberg@psi.ch>
2023-01-11 09:18:39 +0100

    test:test_redis_connector

--- bec_utils/tests/test_redis_connector.py
+++ bec_utils/tests/test_redis_connector.py
@@ -11,6 +11,15 @@ def producer():
         yield prod
 
 
+@pytest.mark.parametrize("topic , msg", [["topic1", "msg1"], ["topic2", "msg2"]])
+def test_redis_producer_send(producer, topic, msg):
+    producer.send(topic, msg)
+    producer.r.publish.assert_called_once_with(f"{topic}:sub", msg)
+
+    producer.send(topic, msg, pipe=producer.pipeline())
+    producer.r.pipeline().publish.assert_called_once_with(f"{topic}:sub", msg)
+
+
 def test_redis_producer_set(producer):
     producer.set("topic", "msg")
     producer.r.pipeline().set.assert_called_once()
