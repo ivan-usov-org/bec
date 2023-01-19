cfd3b19b499af2161a657257e593b2dd71e354e4
Klaus Wakonig <klaus.wakonig@psi.ch>
2023-01-19 11:55:06 +0100

    refactor: cleanup

--- bec_client/setup.py
+++ bec_client/setup.py
@@ -5,8 +5,7 @@ import subprocess
 from setuptools import setup
 
 current_path = pathlib.Path(__file__).parent.resolve()
-os.environ["BEC_CLIENT_PATH"] = str(current_path)
-print(current_path)
+
 utils = f"{current_path}/../bec_utils/"
 
 if __name__ == "__main__":
@@ -28,15 +27,3 @@ if __name__ == "__main__":
     local_deps = [utils]
     for dep in local_deps:
         subprocess.run(f"pip install -e {dep}", shell=True, check=True)
-
-        # install_requires=[
-        #     "numpy==1.23.0",
-        #     "msgpack==1.0.4",
-        #     "requests==2.28.0",
-        #     "typeguard==2.13.3",
-        #     "pyyaml==6.0",
-        #     "redis==4.3.3",
-        #     "ipython==8.4.0",
-        #     "cytoolz==0.11.2",
-        #     "rich==12.4.4",
-        # ]
