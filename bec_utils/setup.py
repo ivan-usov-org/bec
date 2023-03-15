ac984d14c7e94ee506b13a41b632137cd6bf800a
wakonig_k <klaus.wakonig@psi.ch>
2023-03-15 14:23:50 +0000

    Update setup.py

--- bec_utils/setup.py
+++ bec_utils/setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 
 if __name__ == "__main__":
     setup(
-        install_requires=["msgpack", "requests", "typeguard", "pyyaml", "redis", "loguru", "rich"]
+        install_requires=["msgpack", "requests", "typeguard<3.0", "pyyaml", "redis", "loguru", "rich"]
     )
