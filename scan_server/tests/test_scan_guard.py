a94e53e047035bcc3ec9a681eb46985ddf419ce2
Klaus Wakonig <klaus.wakonig@psi.ch>
2023-05-15 14:10:18 +0200

    test: added scan guard tests

--- scan_server/tests/test_scan_guard.py
+++ scan_server/tests/test_scan_guard.py
@@ -66,3 +66,59 @@ def test_device_rpc_is_valid(device, func, is_valid):
 
     sg = ScanGuard(parent=k)
     assert sg._device_rpc_is_valid(device, func) == is_valid
+
+
+@pytest.mark.parametrize(
+    "scan_queue_msg,valid",
+    [
+        (
+            BECMessage.ScanQueueMessage(
+                scan_type="fermat_scan",
+                parameter={
+                    "args": {"samx": (-5, 5), "samy": (-5, 5)},
+                    "kwargs": {"step": 3},
+                },
+                queue="primary",
+            ),
+            True,
+        ),
+        (
+            BECMessage.ScanQueueMessage(
+                scan_type="device_rpc",
+                parameter={
+                    "device": "samy",
+                    "args": {},
+                    "kwargs": {},
+                },
+                queue="primary",
+            ),
+            True,
+        ),
+        (
+            BECMessage.ScanQueueMessage(
+                scan_type="device_rpc",
+                parameter={
+                    "device": ["samy"],
+                    "args": {},
+                    "kwargs": {},
+                },
+                queue="primary",
+            ),
+            True,
+        ),
+    ],
+)
+def test_valid_request(scan_queue_msg, valid):
+    k = load_ScanServerMock()
+
+    sg = ScanGuard(parent=k)
+    config_reply = BECMessage.RequestResponseMessage(accepted=True, message="")
+    with mock.patch.object(
+        k.device_manager.config_helper, "wait_for_config_reply", return_value=config_reply
+    ):
+        with mock.patch.object(sg, "_check_valid_scan") as valid_scan:
+            k.device_manager.devices["samx"].enabled = True
+            k.device_manager.devices["samy"].enabled = True
+            status = sg._is_valid_scan_request(scan_queue_msg)
+            valid_scan.assert_called_once_with(scan_queue_msg)
+            assert status.accepted == valid
