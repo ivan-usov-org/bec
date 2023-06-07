fd746cc8bf4137792a7a33aa5aaec2aef84f1ba0
Klaus Wakonig <klaus.wakonig@psi.ch>
2023-06-07 15:27:13 +0200

    test: improved tests

--- bec_client/tests/client_tests/test_ipython_live_updates.py
+++ bec_client/tests/client_tests/test_ipython_live_updates.py
@@ -1,3 +1,5 @@
+from unittest import mock
+
 import pytest
 from bec_client_lib.core import BECMessage
 from bec_client_lib.core.tests.utils import bec_client
@@ -7,7 +9,7 @@ from bec_client.callbacks.ipython_live_updates import IPythonLiveUpdates
 
 
 @pytest.mark.timeout(20)
-def test_live_updates_process_queue(bec_client):
+def test_live_updates_process_queue_pending(bec_client):
     client = bec_client
     client.start()
     live_updates = IPythonLiveUpdates(client)
@@ -20,10 +22,93 @@ def test_live_updates_process_queue(bec_client):
     queue = QueueItem(
         scan_manager=client.queue,
         queueID="queueID",
-        request_blocks=[],
+        request_blocks=[request_msg],
         status="PENDING",
         active_request_block={},
         scanID=["scanID"],
     )
-    res = live_updates._process_queue(queue, request_msg, "req_id")
-    assert res is False
+    client.queue.queue_storage.current_scan_queue = {"primary": {"status": "RUNNING"}}
+    with mock.patch.object(queue, "_update_with_buffer"):
+        with mock.patch(
+            "bec_client_lib.queue_items.QueueItem.queue_position", new_callable=mock.PropertyMock
+        ) as queue_pos:
+            queue_pos.return_value = 2
+            with mock.patch.object(
+                live_updates,
+                "_available_req_blocks",
+                return_value=[{"report_instructions": [], "content": {"scan_type": "grid_scan"}}],
+            ):
+                with mock.patch.object(live_updates, "_process_report_instructions") as process:
+                    with mock.patch("builtins.print") as prt:
+                        res = live_updates._process_queue(queue, request_msg, "req_id")
+                        prt.assert_called_once()
+                        process.assert_not_called()
+                    assert res is False
+
+
+@pytest.mark.timeout(20)
+def test_live_updates_process_queue_running(bec_client):
+    client = bec_client
+    client.start()
+    live_updates = IPythonLiveUpdates(client)
+    request_msg = BECMessage.ScanQueueMessage(
+        scan_type="grid_scan",
+        parameter={"args": {"samx": (-5, 5, 3)}, "kwargs": {}},
+        queue="primary",
+        metadata={"RID": "something"},
+    )
+    queue = QueueItem(
+        scan_manager=client.queue,
+        queueID="queueID",
+        request_blocks=[request_msg],
+        status="RUNNING",
+        active_request_block={},
+        scanID=["scanID"],
+    )
+    live_updates._active_request = request_msg
+    client.queue.queue_storage.current_scan_queue = {"primary": {"status": "RUNNING"}}
+    with mock.patch.object(queue, "_update_with_buffer"):
+        with mock.patch(
+            "bec_client_lib.queue_items.QueueItem.queue_position", new_callable=mock.PropertyMock
+        ) as queue_pos:
+            queue_pos.return_value = 2
+            with mock.patch.object(
+                live_updates,
+                "_available_req_blocks",
+                return_value=[
+                    {
+                        "report_instructions": [{"wait_table": 10}],
+                        "content": {"scan_type": "grid_scan"},
+                    }
+                ],
+            ):
+                with mock.patch.object(live_updates, "_process_instruction") as process:
+                    with mock.patch("builtins.print") as prt:
+                        res = live_updates._process_queue(queue, request_msg, "req_id")
+                        prt.assert_not_called()
+                        process.assert_called_once_with({"wait_table": 10})
+                    assert res is True
+
+
+# @pytest.mark.timeout(20)
+# @pytest.mark.asyncio
+# def test_live_updates_process_instruction_readback(bec_client):
+#     client = bec_client
+#     client.start()
+#     live_updates = IPythonLiveUpdates(client)
+#     request_msg = BECMessage.ScanQueueMessage(
+#         scan_type="grid_scan",
+#         parameter={"args": {"samx": (-5, 5, 3)}, "kwargs": {}},
+#         queue="primary",
+#         metadata={"RID": "something"},
+#     )
+#     live_updates._active_request = request_msg
+#     live_updates._user_callback = []
+#     client.queue.queue_storage.current_scan_queue = {"primary": {"status": "RUNNING"}}
+#     with mock.patch(
+#         "bec_client.callbacks.ipython_live_updates.LiveUpdatesTable", new_callable=mock.Co
+#     ) as table:
+#         live_updates._process_instruction({"table_wait": 10})
+#         table.assert_called_once_with(
+#             client, report_instructions={"table_wait": 10}, request=request_msg, callbacks=[]
+#         )
