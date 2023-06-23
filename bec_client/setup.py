6669bce3e178ca71d664adf9a7493e7ecad4589d
Klaus Wakonig <klaus.wakonig@psi.ch>
2023-06-23 20:13:10 +0200

    fix: version variable is pulled from semantic release file

--- bec_client/setup.py
+++ bec_client/setup.py
@@ -9,6 +9,16 @@ current_path = pathlib.Path(__file__).parent.resolve()
 utils = f"{current_path}/../bec_client_lib/"
 bec_client_lib = f"{current_path}/../bec_client_lib/"
 
+
+def get_version():
+    """load the version from the version file"""
+    version_file = os.path.join(current_path, "../semantic_release", "__init__.py")
+    with open(version_file, "r", encoding="utf-8") as file:
+        res = file.readline()
+        version = res.split("=")[1]
+    return version.strip().strip('"')
+
+
 if __name__ == "__main__":
     setup(
         install_requires=[
@@ -26,6 +36,7 @@ if __name__ == "__main__":
             "fpdf",
         ],
         scripts=["bec_client/bin/bec"],
+        version=get_version(),
     )
     local_deps = [utils, bec_client_lib]
     for dep in local_deps:
--- bec_client_lib/setup.py
+++ bec_client_lib/setup.py
@@ -1,8 +1,18 @@
+import os
 import pathlib
 
 from setuptools import setup
 
 current_path = pathlib.Path(__file__).parent.resolve()
+
+
+def get_version():
+    """load the version from the version file"""
+    version_file = os.path.join(current_path, "../semantic_release", "__init__.py")
+    with open(version_file, "r", encoding="utf-8") as file:
+        res = file.readline()
+        version = res.split("=")[1]
+    return version.strip().strip('"')
 
 
 if __name__ == "__main__":
@@ -23,4 +33,5 @@ if __name__ == "__main__":
             "psutil",
         ],
         extras_require={"dev": ["pytest", "pytest-random-order", "coverage", "pandas"]},
+        version=get_version(),
     )
