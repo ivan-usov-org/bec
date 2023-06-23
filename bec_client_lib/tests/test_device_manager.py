08e60a0513cedcbc41d61272932b2e6cd412cad1
Klaus Wakonig <klaus.wakonig@psi.ch>
2023-06-23 22:31:45 +0200

    test: added more device_manager tests

--- bec_client_lib/tests/test_device_manager.py
+++ bec_client_lib/tests/test_device_manager.py
@@ -8,7 +8,13 @@ import yaml
 import bec_client_lib
 from bec_client_lib.core import BECMessage
 from bec_client_lib.core.connector import MessageObject
-from bec_client_lib.core.devicemanager import DeviceConfigError, DeviceManagerBase
+from bec_client_lib.core.devicemanager import (
+    Device,
+    DeviceConfigError,
+    DeviceContainer,
+    DeviceManagerBase,
+    Status,
+)
 from bec_client_lib.core.tests.utils import ConnectorMock, create_session_from_config
 
 dir_path = os.path.dirname(bec_client_lib.__file__)
@@ -213,3 +219,147 @@ def test_device_config_update_callback():
     with mock.patch.object(dm, "parse_config_message") as parse_config_message:
         dm._device_config_update_callback(msg, parent=dm)
         parse_config_message.assert_called_once_with(dev_config_msg)
+
+
+def test_status_wait():
+    producer = mock.MagicMock()
+
+    def lrange_mock(*args, **kwargs):
+        yield False
+        yield True
+
+    def get_lrange(*args, **kwargs):
+        return next(lmock)
+
+    lmock = lrange_mock()
+    producer.lrange = get_lrange
+    status = Status(producer, "test")
+    status.wait()
+
+
+def test_device_get_device_config():
+    device = Device("test", {"deviceConfig": {"tolerance": 1}})
+    assert device.get_device_config() == {"tolerance": 1}
+
+
+def test_device_set_device_config():
+    parent = mock.MagicMock()
+    device = Device("test", {"deviceConfig": {"tolerance": 1}}, parent=parent)
+    device.set_device_config({"tolerance": 2})
+    assert device.get_device_config() == {"tolerance": 2}
+    parent.config_helper.send_config_request.assert_called_once()
+
+
+def test_get_device_tags():
+    device = Device("test", {"deviceTags": ["tag1", "tag2"]})
+    assert device.get_device_tags() == ["tag1", "tag2"]
+
+    device = Device("test", {})
+    assert device.get_device_tags() == []
+
+
+def test_set_device_tags():
+    parent = mock.MagicMock()
+    device = Device("test", {"deviceTags": ["tag1", "tag2"]}, parent=parent)
+    device.set_device_tags(["tag3", "tag4"])
+    assert device.get_device_tags() == ["tag3", "tag4"]
+    parent.config_helper.send_config_request.assert_called_once()
+
+
+def test_add_device_tag():
+    parent = mock.MagicMock()
+    device = Device("test", {"deviceTags": ["tag1", "tag2"]}, parent=parent)
+    device.add_device_tag("tag3")
+    assert device.get_device_tags() == ["tag1", "tag2", "tag3"]
+    parent.config_helper.send_config_request.assert_called_once()
+
+
+def test_add_device_tags_duplicate():
+    parent = mock.MagicMock()
+    device = Device("test", {"deviceTags": ["tag1", "tag2"]}, parent=parent)
+    device.add_device_tag("tag1")
+    assert device.get_device_tags() == ["tag1", "tag2"]
+    parent.config_helper.send_config_request.assert_not_called()
+
+
+def test_remove_device_tag():
+    parent = mock.MagicMock()
+    device = Device("test", {"deviceTags": ["tag1", "tag2"]}, parent=parent)
+    device.remove_device_tag("tag1")
+    assert device.get_device_tags() == ["tag2"]
+    parent.config_helper.send_config_request.assert_called_once()
+
+
+def test_device_wm():
+    parent = mock.MagicMock()
+    device = Device("test", {"deviceTags": ["tag1", "tag2"]}, parent=parent)
+    with mock.patch.object(parent.devices, "wm", new_callable=mock.PropertyMock) as wm:
+        res = device.wm
+        parent.devices.wm.assert_called_once()
+
+
+def test_readout_priority():
+    parent = mock.MagicMock()
+    device = Device("test", {"acquisitionConfig": {"readoutPriority": "baseline"}}, parent=parent)
+    assert device.readout_priority == "baseline"
+
+
+def test_set_readout_priority():
+    parent = mock.MagicMock()
+    device = Device("test", {"acquisitionConfig": {"readoutPriority": "baseline"}}, parent=parent)
+    device.readout_priority = "monitored"
+    assert device.readout_priority == "monitored"
+    parent.config_helper.send_config_request.assert_called_once()
+
+
+def test_on_failure():
+    parent = mock.MagicMock()
+    device = Device("test", {"onFailure": "buffer"}, parent=parent)
+    assert device.on_failure == "buffer"
+
+
+def test_set_on_failure():
+    parent = mock.MagicMock()
+    device = Device("test", {"onFailure": "buffer"}, parent=parent)
+    device.on_failure = "retry"
+    assert device.on_failure == "retry"
+    parent.config_helper.send_config_request.assert_called_once()
+
+
+def test_enabled_set():
+    parent = mock.MagicMock()
+    device = Device("test", {"enabled_set": True}, parent=parent)
+    assert device.enabled_set is True
+
+
+def test_set_enabled_set():
+    parent = mock.MagicMock()
+    device = Device("test", {"enabled_set": True}, parent=parent)
+    device.enabled_set = False
+    assert device.enabled_set is False
+    parent.config_helper.send_config_request.assert_called_once()
+
+
+def test_device_container_wm():
+    devs = DeviceContainer()
+    devs["test"] = Device("test", {})
+    with mock.patch.object(devs.test, "read", return_value={"test": {"value": 1}}) as read:
+        devs.wm("test")
+
+
+def test_device_container_wm_with_setpoint():
+    devs = DeviceContainer()
+    devs["test"] = Device("test", {})
+    with mock.patch.object(
+        devs.test, "read", return_value={"test": {"value": 1}, "test_setpoint": {"value": 1}}
+    ) as read:
+        devs.wm("test")
+
+
+def test_device_container_wm_with_user_setpoint():
+    devs = DeviceContainer()
+    devs["test"] = Device("test", {})
+    with mock.patch.object(
+        devs.test, "read", return_value={"test": {"value": 1}, "test_user_setpoint": {"value": 1}}
+    ) as read:
+        devs.wm("test")
