1d801c7feb28a5e286a605aaa78034c0a6f5b785
Klaus Wakonig <klaus.wakonig@psi.ch>
2023-06-27 19:56:26 +0200

    ci: added external shell script for build process

---
+++ ci/build_python_services.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# define all target directories
+declare -a dirs=(
+    "bec_client"
+    "bec_client_lib"
+    "scan_server"
+    "device_server"
+    "scan_bundler"
+    "file_writer"
+    "data_processing"
+)
+
+mkdir dist
+
+# loop over all directories and run the build command
+for dir in "${dirs[@]}"
+do
+    echo "Building $dir"
+    cd $dir
+    python setup.py sdist bdist_wheel
+    cp dist/* ../dist
+    rm -r ./dist
+    cd ..
+done
