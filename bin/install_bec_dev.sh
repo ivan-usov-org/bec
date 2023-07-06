db9539aba203e7e299620f76dfd1f3843ebfecbd
Klaus Wakonig <klaus.wakonig@psi.ch>
2023-07-06 15:03:58 +0200

    feat: added install_bec_dev script

---
+++ bin/install_bec_dev.sh
@@ -0,0 +1,39 @@
+# helper script to (re-)install all BEC dependencies in freshly created virtual environment
+
+conda_env_name="bec_base_env"
+
+# check if conda is installed
+if ! [ -x "$(command -v conda)" ]; then
+    echo "conda is not installed. Please install conda first."
+    exit 1
+fi
+
+# check if conda environment exists and install it if not
+if ! conda env list | grep -q ${conda_env_name}; then
+    echo "Creating conda environment ${conda_env_name}..."
+    conda create --name ${conda_env_name} python=3.8 redis
+fi
+
+for i in $(seq ${CONDA_SHLVL}); do
+    conda deactivate
+done
+
+dependencies=(bec_lib scan_server scan_bundler data_processing file_writer device_server scihub bec_client)
+
+# loop over all packages and install them
+for package in "${dependencies[@]}"
+do
+    echo "Installing $package..."
+    conda activate ${conda_env_name}
+    cd ./$package
+    rm -rf ${package}_venv
+    python -m venv ./${package}_venv
+    conda deactivate    
+    source ./${package}_venv/bin/activate
+    pip install -q -q -e .
+    cd ../
+    echo "\r\rInstalled $package"
+done
+
+
+
