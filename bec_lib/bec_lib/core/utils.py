71188638323f27f0ae7f643a0e8b3ade12579899
appel_c <christian.appel@psi.ch>
2023-07-26 20:23:02 +0200

    fix: adapt write_to_csv to write multiple scan_reports for context manager

--- bec_lib/bec_lib/core/utils.py
+++ bec_lib/bec_lib/core/utils.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 from typeguard import typechecked
 import functools
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, List, Union
 from collections import defaultdict
 import csv
 
@@ -21,8 +21,9 @@ def threadlocked(fcn):
     return wrapper
 
 
+@typechecked
 def scan_to_csv(
-    scan_report: ScanReport,
+    scan_report: Union[ScanReport, List[ScanReport]],
     output_name: str,
     delimiter: str = ",",
     dialect: str = None,
@@ -42,33 +43,21 @@ def scan_to_csv(
         Examples:
             >>> to_csv(scan_report, "./scan.csv")
     """
-    scan_dict = scan_to_dict(scan_report, flat=True)
-    data_output = [["#" + entry.replace("\t", "")] for entry in str(scan_report).split("\n")]
-    if write_metadata:
-        data_output.append(["#ScanMetadata"])
-        scan_metadata = scan_report.scan.data[list(scan_report.scan.data.keys())[-1]].metadata
-        for key, value in scan_metadata.items():
-            data_output.append(["".join(["#", key]), value])
-    if header:
-        header_keys = header
-    else:
-        header_keys = []
-        [
-            header_keys.extend([f"{value}_value", f"{time}_timestamp"])
-            for value, time in zip(scan_dict["value"].keys(), scan_dict["timestamp"].keys())
-        ]
-    header_keys[0] = "".join(["#", header_keys[0]])
-    data_output.append(header_keys)
+    if isinstance(scan_report, ScanReport):
+        scan_report = [scan_report]
+    header_out = []
+    body_out = []
+    data_output = []
 
-    data = []
-    num_entries = len(list(scan_dict["value"].values())[0])
-    for ii in range(num_entries):
-        sub_list = []
-        for key in scan_dict["value"]:
-            sub_list.extend([scan_dict["value"][key][ii], scan_dict["timestamp"][key][ii]])
-        data.append(sub_list)
-
-    data_output.extend(data)
+    for scan_rep in scan_report:
+        header_tmp, body_tmp = _extract_scan_data(
+            scan_report=scan_rep, header=header, write_metadata=write_metadata
+        )
+        header_out.extend(header_tmp[:-1])
+        body_out.extend(body_tmp)
+    header_out.append(header_tmp[-1])  # To only append the header keys once
+    data_output.extend(header_out)
+    data_output.extend(body_out)
 
     _write_csv(
         output_name=output_name,
@@ -78,7 +67,6 @@ def scan_to_csv(
     )
 
 
-@typechecked
 def _write_csv(
     output_name: str,
     delimiter: str,
@@ -100,6 +88,52 @@ def _write_csv(
     with open(output_name, "w", encoding="UTF-8") as file:
         writer = csv.writer(file, delimiter=delimiter, dialect=dialect)
         writer.writerows(output)
+
+
+def _extract_scan_data(
+    scan_report: ScanReport,
+    header: list = None,
+    write_metadata: bool = True,
+) -> tuple:
+    """Extract scan data from scan report.
+
+    Args:
+        scan_report (ScanReport): Scan report object.
+        header (list, optional): Create custom header for the csv file. If None, header is created automatically. Defaults to None.
+        write_metadata (bool, optional): If True, the metadata of the scan will be written to the header of csv file. Defaults to True.
+
+    Returns:
+        (tuple): Tuple of header and body of the csv file.
+    """
+    scan_dict = scan_to_dict(scan_report, flat=True)
+
+    header_tmp = [["#" + entry.replace("\t", "")] for entry in str(scan_report).split("\n")]
+    if write_metadata:
+        header_tmp.append(["#ScanMetadata"])
+        scan_metadata = scan_report.scan.data[list(scan_report.scan.data.keys())[-1]].metadata
+        for key, value in scan_metadata.items():
+            header_tmp.append(["".join(["#", key]), value])
+    if header:
+        header_keys = header
+    else:
+        header_keys = ["scan_number", "dataset_number"]
+        [
+            header_keys.extend([f"{value}_value", f"{time}_timestamp"])
+            for value, time in zip(scan_dict["value"].keys(), scan_dict["timestamp"].keys())
+        ]
+    header_keys[0] = "".join(["#", header_keys[0]])
+    header_tmp.append(header_keys)
+
+    body_tmp = []
+    num_entries = len(list(scan_dict["value"].values())[0])
+    for ii in range(num_entries):
+        sub_list = []
+        sub_list.extend([scan_metadata["scan_number"], scan_metadata["dataset_number"]])
+        for key in scan_dict["value"]:
+            sub_list.extend([scan_dict["value"][key][ii], scan_dict["timestamp"][key][ii]])
+        body_tmp.append(sub_list)
+
+    return header_tmp, body_tmp
 
 
 def scan_to_dict(scan_report: ScanReport, flat: bool = True) -> dict:
