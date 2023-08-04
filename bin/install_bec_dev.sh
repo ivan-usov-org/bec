588968e2fa29f25d2f2ced59821e7d57ef3e1cf9
Klaus Wakonig <klaus.wakonig@psi.ch>
2023-08-04 11:43:42 +0200

    build: added option to install bec without redis / tmux

--- bin/install_bec_dev.sh
+++ bin/install_bec_dev.sh
@@ -4,20 +4,29 @@
 # -h: help
 # -s: split virtual environment into separate environments for each package
 # -c: conda environment name (default: bec_base_env)
+# -r: skip installation of redis (default: false)
+# -t: skip installation of tmux (default: false)
+
+OPTIND=1
 
 # default values
 split_env=false
 conda_env_name="bec_base_env"
+skip_redis=false
+skip_tmux=false
 
-while getopts "hsc:" opt; do
-    case ${opt} in
+
+while getopts "hsc:rt" o; do
+    case "${o}" in
         h)
-            echo "Usage: install_bec_dev.sh [-h] [-s] [-c <conda_env_name>]"
+            echo "Usage: install_bec_dev.sh [-h] [-s] [-c conda_env_name] [-r] [-t]"
             echo "Options:"
             echo "-h: help"
             echo "-s: split virtual environment into separate environments for each package"
             echo "-c: conda environment name (default: bec_base_env)"
-            exit 0
+            echo "-r: skip installation of redis (default: false)"
+            echo "-t: skip installation of tmux (default: false)"
+            return
             ;;
         s)
             split_env=true
@@ -25,12 +34,44 @@ while getopts "hsc:" opt; do
         c)
             conda_env_name=$OPTARG
             ;;
+        r)
+            skip_redis=true
+            ;;
+        t)
+            echo "skip tmux"
+            skip_tmux=true
+            ;;
         \?)
             echo "Invalid option: -$OPTARG" >&2
-            exit 1
+            return
             ;;
     esac
 done
+
+conda_deps=(python=3.8)
+
+# check if tmux should be installed
+if [ "$skip_tmux" = false ]; then
+    # add tmux to dependencies
+    conda_deps+=(tmux)
+else
+    # check if tmux is installed
+    if ! [ "$(which tmux)" ]; then
+        echo "tmux is not installed. Please install tmux."
+    fi
+fi
+
+# check if redis should be installed
+if [ "$skip_redis" = false ]; then
+    # add redis to dependencies
+    conda_deps+=(redis)
+else
+    # check if redis is installed
+    if ! [ "$(which redis-server)" ]; then
+        echo "redis is not installed. Please install redis."
+    fi
+fi
+
 
 # check if conda is installed
 if ! [ "$(which conda)" ]; then
@@ -38,10 +79,12 @@ if ! [ "$(which conda)" ]; then
     return
 fi
 
+echo "Installing with conda dependencies: ${conda_deps[@]}"
+
 # check if conda environment exists and install it if not
 if ! conda env list | grep -q ${conda_env_name}; then
     echo "Creating conda environment ${conda_env_name}..."
-    conda create --name ${conda_env_name} python=3.8 redis
+    conda create --name ${conda_env_name} ${conda_deps[@]}
 fi
 
 for i in $(seq ${CONDA_SHLVL}); do
