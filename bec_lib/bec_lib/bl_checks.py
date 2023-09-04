e9d0fbe44403013430380bd1a8f98d951bbb25be
wakonig_k <klaus.wakonig@psi.ch>
2023-09-04 19:25:14 +0200

    refactor: refactored beamline checks to simplify unit tests

--- bec_lib/bec_lib/bl_checks.py
+++ bec_lib/bec_lib/bl_checks.py
@@ -1,20 +1,16 @@
 import builtins
 import datetime
+import functools
 import threading
 import time
-from abc import ABC, abstractmethod
-import builtins
-import functools
-import inspect
 from collections import deque
-
+from typing import Tuple
 from uuid import uuid4
-
 
 from typeguard import typechecked
 
+from bec_lib.bl_conditions import BeamlineCondition
 from bec_lib.core import bec_logger
-from bec_lib.core.devicemanager import Device
 
 logger = bec_logger.logger
 
@@ -30,118 +26,49 @@ def bl_check(fcn):
     def bl_check_wrapper(*args, **kwargs):
         client = builtins.__dict__.get("bec")
         bl_checks = client.bl_checks
-        chk = {"id": str(uuid4()), "fcn": fcn, "args": args, "kwargs": kwargs}
-        bl_checks._levels.append(chk)
-        nested_call = len(bl_checks._levels) > 1
-        if bl_checks._is_paused and bl_checks._beamline_checks:
-            logger.warning(
-                f"Beamline checks are currently paused. Use `bec.bl_checks.resume()` to reactivate them."
-            )
-        try:
-            if nested_call:
-                # check if the beam was okay so far
-                if not bl_checks.beam_is_okay:
-                    raise BeamlineCheckError("Beam is not okay.")
-            else:
-                bl_checks.reset()
-                bl_checks.wait_for_beamline_checks()
-            successful = False
-            while not successful:
-                try:
-                    res = fcn(*args, **kwargs)
-                except BeamlineCheckError:
-                    successful = False
-                    bl_checks.wait_for_beamline_checks()
-                    continue
-
-                if bl_checks.beam_is_okay:
-                    successful = True
-                else:
-                    bl_checks.wait_for_beamline_checks()
-            return res
-
-        finally:
-            bl_checks._levels.pop()
+        _run_with_bl_checks(bl_checks, fcn, *args, **kwargs)
 
     return bl_check_wrapper
 
 
-class BeamlineCondition(ABC):
-    """Abstract base class for beamline checks."""
+def _run_with_bl_checks(bl_checks, fcn, *args, **kwargs):
+    # pylint: disable=protected-access
+    chk = {"id": str(uuid4()), "fcn": fcn, "args": args, "kwargs": kwargs}
+    bl_checks._levels.append(chk)
+    nested_call = len(bl_checks._levels) > 1
+    if bl_checks._is_paused and bl_checks._beamline_checks:
+        logger.warning(
+            f"Beamline checks are currently paused. Use `bec.bl_checks.resume()` to reactivate them."
+        )
+    try:
+        if nested_call:
+            # check if the beam was okay so far
+            if not bl_checks.beam_is_okay:
+                raise BeamlineCheckError("Beam is not okay.")
+        else:
+            bl_checks.reset()
+            bl_checks.wait_for_beamline_checks()
+        successful = False
+        while not successful:
+            successful, res = _run_on_failure(bl_checks, fcn, args, kwargs)
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.enabled = True
+            if bl_checks.beam_is_okay:
+                successful = True
+            else:
+                bl_checks.wait_for_beamline_checks()
+        return res
 
-    @property
-    @abstractmethod
-    def name(self) -> str:
-        """Return a name for the beamline check."""
-
-    @abstractmethod
-    def run(self) -> bool:
-        """Run the beamline check and return True if the beam is okay, False otherwise."""
-
-    @abstractmethod
-    def on_failure_msg(self) -> str:
-        """Return a message that will be displayed if the beamline check fails."""
-
-
-class ShutterCondition(BeamlineCondition):
-    """Check if the shutter is open."""
-
-    def __init__(self, shutter: Device):
-        super().__init__()
-        self.shutter = shutter
-
-    @property
-    def name(self):
-        return "shutter"
-
-    def run(self):
-        shutter_val = self.shutter.read(cached=True)
-        return shutter_val["value"].lower() == "open"
-
-    def on_failure_msg(self):
-        return "Check beam failed: Shutter is closed."
+    finally:
+        bl_checks._levels.pop()
 
 
-class LightAvailableCondition(BeamlineCondition):
-    """Check if the light is available."""
-
-    def __init__(self, machine_status: Device):
-        super().__init__()
-        self.machine_status = machine_status
-
-    @property
-    def name(self):
-        return "light_available"
-
-    def run(self):
-        machine_status = self.machine_status.read(cached=True)
-        return machine_status["value"] in ["Light Available", "Light-Available"]
-
-    def on_failure_msg(self):
-        return "Check beam failed: Light not available."
-
-
-class FastOrbitFeedbackCondition(BeamlineCondition):
-    """Check if the fast orbit feedback is running."""
-
-    def __init__(self, sls_fast_orbit_feedback: Device):
-        super().__init__()
-        self.sls_fast_orbit_feedback = sls_fast_orbit_feedback
-
-    @property
-    def name(self):
-        return "fast_orbit_feedback"
-
-    def run(self):
-        fast_orbit_feedback = self.sls_fast_orbit_feedback.read(cached=True)
-        return fast_orbit_feedback["value"] == "running"
-
-    def on_failure_msg(self):
-        return "Check beam failed: Fast orbit feedback is not running."
+def _run_on_failure(bl_checks, fcn, *args, **kwargs) -> Tuple:
+    try:
+        res = fcn(*args, **kwargs)
+        return (True, res)
+    except BeamlineCheckError:
+        bl_checks.wait_for_beamline_checks()
+        return (False, None)
 
 
 class BeamlineChecks:
@@ -279,18 +206,24 @@ class BeamlineChecks:
                 pen="red",
             )
 
-        while True:
-            self._beam_is_okay = True
-            self._check_msgs = self._run_beamline_checks()
-            if self.beam_is_okay:
-                break
-            self._print_beamline_checks()
-            time.sleep(5)
+        self._run_beamline_checks_until_okay()
 
         if self.send_to_scilog:
             self._send_to_scilog(
                 f"Operation resumed at {str(datetime.datetime.now())}.", pen="green"
             )
+
+    def _run_beamline_checks_until_okay(self):
+        while True:
+            if self._beam_status_is_okay():
+                break
+            self._print_beamline_checks()
+            time.sleep(5)
+
+    def _beam_status_is_okay(self) -> bool:
+        self._beam_is_okay = True
+        self._check_msgs = self._run_beamline_checks()
+        return self._beam_is_okay
 
     def _send_to_scilog(self, msg, pen="red"):
         try:
