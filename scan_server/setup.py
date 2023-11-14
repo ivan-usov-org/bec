45b6a937b7333f1d96fee14a22550b51cd3f9be2
wakonig_k <klaus.wakonig@psi.ch>
2023-11-14 19:11:24 +0100

    refactor: improved setup.py

--- scan_server/setup.py
+++ scan_server/setup.py
@@ -12,8 +12,6 @@ bec_lib = f"{current_path}/../bec_lib/"
 __version__ = "0.42.3"
 
 
-
-
 def run_install(setup_args: dict, bec_deps: list, editable=False):
     """
     Run the setup function with the given arguments.
@@ -42,17 +40,17 @@ def run_install(setup_args: dict, bec_deps: list, editable=False):
 
 
 if __name__ == "__main__":
-    setup_args = {
+    setup_args_in = {
         "entry_points": {"console_scripts": ["bec-scan-server = scan_server:main"]},
         "install_requires": ["numpy", "msgpack", "pyyaml", "cytoolz", "rich"],
         "version": __version__,
         "extras_require": {"dev": ["pytest", "pytest-random-order", "coverage", "black", "pylint"]},
     }
-    bec_deps = [
+    bec_deps_in = [
         ("bec_lib", "bec_lib", bec_lib),
     ]
     is_local = os.path.dirname(os.path.abspath(__file__)).split("/")[-1] == "scan_server"
     is_build = "bdist_wheel" in sys.argv
 
-    editable = is_local and not is_build
-    run_install(setup_args, bec_deps, editable=editable)
+    editable_in = is_local and not is_build
+    run_install(setup_args_in, bec_deps_in, editable=editable_in)
