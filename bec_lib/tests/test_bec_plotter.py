d6d888f197e4f61bf825a8bc4ed8db47ca3d61bb
wyzula-jan <133381102+wyzula-jan@users.noreply.github.com>
2023-12-20 19:58:11 +0100

    test: test_bec_plotter.py all tests fixed

--- bec_lib/tests/test_bec_plotter.py
+++ bec_lib/tests/test_bec_plotter.py
@@ -13,7 +13,7 @@ def bec_client():
 
 
 def test_bec_widgets_connector_set_plot_config(bec_client):
-    connector = BECWidgetsConnector(bec_client)
+    connector = BECWidgetsConnector(gui_id="gui_id", bec_client=bec_client)
     config = {"x": "test", "y": "test", "color": "test", "size": "test", "shape": "test"}
     connector.set_plot_config(plot_id="plot_id", config=config)
     msg = messages.GUIConfigMessage(config=config).dumps()
@@ -23,7 +23,7 @@ def test_bec_widgets_connector_set_plot_config(bec_client):
 
 
 def test_bec_widgets_connector_close(bec_client):
-    connector = BECWidgetsConnector(bec_client)
+    connector = BECWidgetsConnector(gui_id="gui_id", bec_client=bec_client)
     connector.close("plot_id")
     msg = messages.GUIInstructionMessage(action="close", parameter={}).dumps()
     bec_client.connector.producer().set_and_publish.assert_called_once_with(
@@ -32,17 +32,17 @@ def test_bec_widgets_connector_close(bec_client):
 
 
 def test_bec_widgets_connector_send_data(bec_client):
-    connector = BECWidgetsConnector(bec_client)
+    connector = BECWidgetsConnector(gui_id="gui_id", bec_client=bec_client)
     data = {"x": [1, 2, 3], "y": [1, 2, 3]}
     connector.send_data("plot_id", data)
-    msg = messages.GUIDataMessage(data=data).dumps()
-    bec_client.connector.producer().xadd.assert_called_once_with(
-        MessageEndpoints.gui_data("plot_id"), {"data": msg}
+    msg = messages.GUIDataMessage(data=data)
+    bec_client.connector.producer().set_and_publish.assert_called_once_with(
+        topic=MessageEndpoints.gui_data("plot_id"), msg=msg.dumps()
     )
 
 
 def test_bec_widgets_connector_clear(bec_client):
-    connector = BECWidgetsConnector(bec_client)
+    connector = BECWidgetsConnector(gui_id="gui_id", bec_client=bec_client)
     connector.clear("plot_id")
     msg = messages.GUIInstructionMessage(action="clear", parameter={}).dumps()
     bec_client.connector.producer().set_and_publish.assert_called_once_with(
@@ -52,7 +52,7 @@ def test_bec_widgets_connector_clear(bec_client):
 
 @pytest.fixture
 def plotter(bec_client):
-    widget_connector = BECWidgetsConnector(bec_client)
+    widget_connector = BECWidgetsConnector(gui_id="gui_id", bec_client=bec_client)
     plotter = BECPlotter("test", widget_connector=widget_connector)
     yield plotter
 
@@ -74,69 +74,73 @@ def test_bec_plotter_show_process_not_reset(plotter):
 
 
 def test_bec_plotter_refresh_config_changed(plotter):
-    with mock.patch.object(plotter, "plot_connector") as plot_connector:
+    with mock.patch.object(plotter.plot_connector, "set_plot_config") as mock_set_plot_config:
         plotter._config_changed = True
         plotter.refresh()
-        plot_connector.set_plot_config.assert_called_once_with(plotter._plot_id, plotter._config)
+        mock_set_plot_config.assert_called_once_with(plotter._plot_id, plotter._config)
 
 
 def test_bec_plotter_refresh_config_didnt_change(plotter):
-    with mock.patch.object(plotter, "plot_connector") as plot_connector:
+    with mock.patch.object(plotter.plot_connector, "set_plot_config") as mock_set_plot_config:
         plotter._config_changed = False
         plotter.refresh()
-        plot_connector.set_plot_config.assert_not_called()
+        mock_set_plot_config.assert_not_called()
 
 
 def test_bec_plotter_refresh_data_changed(plotter):
-    with mock.patch.object(plotter, "plot_connector") as plot_connector:
+    with mock.patch.object(plotter.plot_connector, "send_data") as mock_send_data:
         plotter._data_changed = True
+        plotter._xdata = {"data": [1, 2, 3], "action": "set"}  # Example x data
+        plotter._ydata = {"y_tag": {"data": [4, 5, 6], "action": "set"}}  # Example y data
         data = {"x": plotter._xdata, "y": plotter._ydata}
+
         plotter.refresh()
-        plot_connector.send_data.assert_called_once_with(plotter._plot_id, data)
+
+        mock_send_data.assert_called_once_with(plotter._plot_id, data)
 
 
 def test_bec_plotter_refresh_data_didnt_change(plotter):
-    with mock.patch.object(plotter, "plot_connector") as plot_connector:
+    with mock.patch.object(plotter.plot_connector, "send_data") as mock_send_data:
         plotter._data_changed = False
         plotter.refresh()
-        plot_connector.send_data.assert_not_called()
+        mock_send_data.assert_not_called()
 
 
 def test_bec_plotter_clear(plotter):
-    with mock.patch.object(plotter, "plot_connector") as plot_connector:
+    with mock.patch.object(plotter.plot_connector, "clear") as mock_clear:
         plotter.clear()
-        plot_connector.clear.assert_called_once_with(plotter._plot_id)
+        mock_clear.assert_called_once_with(plotter._plot_id)
 
 
 def test_bec_plotter_close(plotter):
-    with mock.patch.object(plotter, "plot_connector") as plot_connector:
+    with mock.patch.object(plotter.plot_connector, "close") as mock_close:
         process = mock.MagicMock()
         plotter._process = process
         plotter.close()
-        plot_connector.close.assert_called_once_with(plotter._plot_id)
+        mock_close.assert_called_once_with(plotter._plot_id)
         process.kill.assert_called_once()
         assert plotter._process is None
 
 
 def test_bec_plotter_close_process_not_called(plotter):
-    with mock.patch.object(plotter, "plot_connector") as plot_connector:
+    with mock.patch.object(plotter.plot_connector, "close") as mock_close:
         plotter._process = None
         plotter.close()
-        plotter.plot_connector.close.assert_not_called()
+        mock_close.assert_not_called()
 
 
 def test_bec_plotter_set_xdata(plotter):
     with mock.patch.object(plotter, "_set_source_to_redis") as set_source_to_redis:
         plotter.set_xdata([1, 2, 3])
-        set_source_to_redis.assert_called_once_with("x")
-        assert plotter._xdata == {"action": "set", "data": [1, 2, 3]}
+        set_source_to_redis.assert_called_once_with("x", "x_default_tag", 0)
+        assert plotter._xdata == {"action": "set", "data": [1, 2, 3], "tag": "x_default_tag"}
 
 
 def test_bec_plotter_set_ydata(plotter):
     with mock.patch.object(plotter, "_set_source_to_redis") as set_source_to_redis:
         plotter.set_ydata([1, 2, 3])
-        set_source_to_redis.assert_called_once_with("y", 0)
-        assert plotter._ydata == {0: {"action": "set", "data": [1, 2, 3]}}
+        set_source_to_redis.assert_called_once_with("y", "y_default_tag", 0)
+        assert plotter._ydata == {"y_default_tag": {"action": "set", "data": [1, 2, 3]}}
 
 
 def test_bec_plotter_set_xsource(plotter):
@@ -167,54 +171,45 @@ def test_bec_plotter_set_ylabel(plotter):
     assert plotter._config_changed is True
 
 
-# def test_bec_plotter_set_source_to_redis(plotter):
-#     config = plotter._config["plot_data"][0]["x"]["signals"][0]
-#     config["source"] = "scan_segment"
-#     plotter._set_source_to_redis("x")
-#     assert plotter._config_changed is True
-#     assert config["source"] == "redis"
-#     assert config["endpoint"] == MessageEndpoints.gui_data(plotter._plot_id)
-#
-#
-# def test_bec_plotter_set_source_to_redis_no_change(plotter):
-#     config = plotter._config["plot_data"][0]["x"]["signals"][0]
-#     config["source"] = "redis"
-#     config["endpoint"] = MessageEndpoints.gui_data(plotter._plot_id)
-#     plotter._set_source_to_redis("x")
-#     assert plotter._config_changed is False
+def test_bec_plotter_set_source_to_redis(plotter):
+    config = plotter._config["plot_data"][0]["sources"]
+    plotter._set_source_to_redis("x")
+    assert plotter._config_changed is True
+    assert config[1]["type"] == "redis"
+    assert config[1]["endpoint"] == MessageEndpoints.gui_data(plotter._plot_id)
 
 
 def test_bec_plotter_append_xdata(plotter):
     with mock.patch.object(plotter, "_set_source_to_redis") as set_source_to_redis:
         plotter.append_xdata([1, 2, 3])
-        set_source_to_redis.assert_called_once_with("x")
-        assert plotter._xdata == {"action": "append", "data": [1, 2, 3]}
+        set_source_to_redis.assert_called_once_with("x", "x_default_tag", 0)
+        assert plotter._xdata == {"action": "append", "data": [1, 2, 3], "tag": "x_default_tag"}
         assert plotter._data_changed is True
 
 
 def test_bec_plotter_append_ydata(plotter):
     with mock.patch.object(plotter, "_set_source_to_redis") as set_source_to_redis:
         plotter.append_ydata([1, 2, 3])
-        set_source_to_redis.assert_called_once_with("y", 0)
-        assert plotter._ydata == {0: {"action": "append", "data": [1, 2, 3]}}
+        set_source_to_redis.assert_called_once_with("y", "y_default_tag", 0)
+        assert plotter._ydata == {"y_default_tag": {"action": "append", "data": [1, 2, 3]}}
         assert plotter._data_changed is True
 
 
 def test_bec_plotter_set_xydata(plotter):
     with mock.patch.object(plotter, "_set_source_to_redis") as set_source_to_redis:
         plotter.set_xydata([1, 2, 3], [1, 2, 3])
-        assert plotter._xdata == {"action": "set", "data": [1, 2, 3]}
+        assert plotter._xdata == {"action": "set", "data": [1, 2, 3], "tag": "x_default_tag"}
         assert plotter._data_changed is True
-        assert plotter._ydata == {0: {"action": "set", "data": [1, 2, 3]}}
+        assert plotter._ydata == {"y_default_tag": {"action": "set", "data": [1, 2, 3]}}
         assert plotter._data_changed is True
 
 
 def test_bec_plotter_append_xydata(plotter):
     with mock.patch.object(plotter, "_set_source_to_redis") as set_source_to_redis:
         plotter.append_xydata([1, 2, 3], [1, 2, 3])
-        assert plotter._xdata == {"action": "append", "data": [1, 2, 3]}
+        assert plotter._xdata == {"action": "append", "data": [1, 2, 3], "tag": "x_default_tag"}
         assert plotter._data_changed is True
-        assert plotter._ydata == {0: {"action": "append", "data": [1, 2, 3]}}
+        assert plotter._ydata == {"y_default_tag": {"action": "append", "data": [1, 2, 3]}}
         assert plotter._data_changed is True
 
 
