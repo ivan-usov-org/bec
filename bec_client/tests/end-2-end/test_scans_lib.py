a8204ac84820562374d335cbbb5584ad14d879af
appel_c <christian.appel@psi.ch>
2024-02-07 19:53:47 +0100

    test: add modified test including .put from client

--- bec_client/tests/end-2-end/test_scans_lib.py
+++ bec_client/tests/end-2-end/test_scans_lib.py
@@ -115,6 +115,18 @@ def test_config_updates(lib_client):
     assert dev.samx.read_configuration()["samx_velocity"]["value"] == 10
     assert dev.samx.read_configuration(cached=False)["samx_velocity"]["value"] == 10
 
+    dev.samx.velocity.put(5)
+    assert dev.samx.velocity.get() == 5
+
+    dev.samx.velocity.set(10)
+    assert dev.samx.velocity.get() == 10
+
+    dev.samx.setpoint.put(5)
+    assert dev.samx.setpoint.get() == 5
+
+    dev.samx.setpoint.set(10)
+    assert dev.samx.setpoint.get() == 10
+
     assert dev.samx.dummy_controller.some_var == 10
     dev.samx.dummy_controller.some_var = 20
     assert dev.samx.dummy_controller.some_var == 20
