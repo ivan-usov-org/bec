3826d410527177acce339e307a17c2943a921aa4
wakonig_k <klaus.wakonig@psi.ch>
2024-02-22 19:46:53 +0100

    fix(bec_lib): fixed service id assignment

--- bec_lib/bec_lib/bec_service.py
+++ bec_lib/bec_lib/bec_service.py
@@ -42,6 +42,7 @@ class BECService:
         self._unique_service = unique_service
         self.wait_for_server = wait_for_server
         self.producer = self.connector.producer()
+        self._service_id = str(uuid.uuid4())
         self._user = getpass.getuser()
         self._hostname = socket.gethostname()
         self._service_info_thread = None
@@ -57,8 +58,12 @@ class BECService:
         self._wait_for_server()
 
     @property
-    def _service_id(self) -> str:
-        return str(uuid.uuid4())
+    def _service_name(self):
+        return (
+            self.__class__.__name__
+            if self._unique_service
+            else f"{self.__class__.__name__}/{self._service_id}"
+        )
 
     def _import_config(self, config: str | ServiceConfig) -> None:
         if isinstance(config, str):
@@ -118,11 +123,7 @@ class BECService:
         self.producer.set_and_publish(
             topic=MessageEndpoints.service_status(self._service_id),
             msg=messages.StatusMessage(
-                name=(
-                    self.__class__.__name__
-                    if self._unique_service
-                    else f"{self.__class__.__name__}/{self._service_id}"
-                ),
+                name=self._service_name,
                 status=self.status,
                 info={"user": self._user, "hostname": self._hostname, "timestamp": time.time()},
             ),
