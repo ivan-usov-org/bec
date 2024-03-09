04d8f1b0d5102212a53c2ee7e47d6169209812cf
wakonig_k <klaus.wakonig@psi.ch>
2024-03-09 12:51:54 +0100

    test: fixed test

--- scan_server/tests/test_flomni_fermat_scan.py
+++ scan_server/tests/test_flomni_fermat_scan.py
@@ -43,21 +43,21 @@ def test_flomni_rotation_no_rotation_required(scan_request):
 
 def test_flomni_rotation_rotation_required(scan_request):
     with mock.patch.object(scan_request.stubs, "send_rpc_and_wait") as send_rpc_mock:
-        send_rpc_mock.return_value = 0
+        send_rpc_mock.iter.return_value = 0
         with mock.patch.object(scan_request.stubs, "scan_report_instruction") as scan_report_mock:
             with mock.patch.object(scan_request.stubs, "set") as set_mock:
-                scan_request.flomni_rotation(90)
-                assert send_rpc_mock.called_once_with("fsamroy", "user_setpoint.get")
-                assert scan_report_mock.called_once_with(
+                list(scan_request.flomni_rotation(90))
+                send_rpc_mock.assert_called_once_with("fsamroy", "user_setpoint.get")
+                scan_report_mock.assert_called_once_with(
                     {
                         "readback": {
                             "RID": scan_request.metadata["RID"],
                             "devices": ["fsamroy"],
-                            "start": [0],
+                            "start": [None],
                             "end": [90],
                         }
                     }
                 )
-                assert set_mock.called_once_with(
+                set_mock.assert_called_once_with(
                     device="fsamroy", value=90, wait_group="flomni_rotation"
                 )
