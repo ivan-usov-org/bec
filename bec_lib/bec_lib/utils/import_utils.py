0ccd13cd738dc12d4a587b4c5e0d6b447d7cfc50
Mathias Guijarro <mathias.guijarro@psi.ch>
2024-05-10 11:22:02 +0200

    feat: add utility function to determine instance of an object by class name

--- bec_lib/bec_lib/utils/import_utils.py
+++ bec_lib/bec_lib/utils/import_utils.py
@@ -1,5 +1,7 @@
+import inspect
 import sys
 from importlib import import_module
+from typing import Any
 
 from bec_lib.utils.proxy import Proxy
 
@@ -14,3 +16,13 @@ def lazy_import_from(module_name, from_list):
         return next(ret)
     else:
         return ret
+
+
+def isinstance_based_on_class_name(obj: Any, full_class_name: str):
+    """Return if object 'obj' is an instance of class named 'full_class_name'
+
+    'full_class_name' must be a string like 'class_module.class_name', the corresponding class does not need to be imported at the caller module level
+    """
+    return full_class_name in [
+        f"{klass.__module__}.{klass.__name__}" for klass in inspect.getmro(type(obj))
+    ]
