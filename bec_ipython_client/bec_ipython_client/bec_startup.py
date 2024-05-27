1b973d2f6bf0a9a60a2c7a35acb4e5a4fd8b580b
Mathias Guijarro <mathias.guijarro@psi.ch>
2024-05-27 15:34:20 +0200

    feat: add --flint option at bec client startup

--- bec_ipython_client/bec_ipython_client/bec_startup.py
+++ bec_ipython_client/bec_ipython_client/bec_startup.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 import sys
 import threading
 
@@ -76,6 +77,18 @@ else:
         gui = bec.gui = GuiProxy(get_gui(_gui, _gui._gui_started_event))
 
         del _gui
+
+    if _main_dict["args"].flint:
+        redis_data_url = f"redis://{bec._client._service_config.redis_data}"
+        flint_process = subprocess.Popen(
+            [sys.executable, "-m", "flint", "--no-rpc", "--no-persistence", "-s", "bec"],
+            env=dict(os.environ) | {"REDIS_DATA_URL": redis_data_url},
+            cwd=os.environ[
+                "HOME"
+            ],  # prevent unexpected imports from current directory (e.g when developing - starting from Python 3.11 can use -P option)
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.STDOUT,
+        )
 
     _available_plugins = plugin_helper.get_ipython_client_startup_plugins(state="post")
     if _available_plugins:
