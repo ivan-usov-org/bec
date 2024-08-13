b0bc0da54f66e5ad4d26471c88eb7d1c8910bead
wakonig_k <klaus.wakonig@psi.ch>
2024-08-13 10:39:43 +0200

    refactor(scan_bundler): moved specific bec emitter methods from emitterbase to bec emitter

--- bec_server/bec_server/scan_bundler/bec_emitter.py
+++ bec_server/bec_server/scan_bundler/bec_emitter.py
@@ -1,5 +1,8 @@
 from __future__ import annotations
 
+import threading
+import time
+from queue import Queue
 from typing import TYPE_CHECKING
 
 from bec_lib import messages
@@ -17,7 +20,52 @@ if TYPE_CHECKING:
 class BECEmitter(EmitterBase):
     def __init__(self, scan_bundler: ScanBundler) -> None:
         super().__init__(scan_bundler.connector)
+        self._send_buffer = Queue()
         self.scan_bundler = scan_bundler
+        self._buffered_connector_thread = None
+        self._buffered_publisher_stop_event = threading.Event()
+        self._start_buffered_connector()
+
+    def _start_buffered_connector(self):
+        self._buffered_connector_thread = threading.Thread(
+            target=self._buffered_publish, daemon=True, name="buffered_publisher"
+        )
+        self._buffered_connector_thread.start()
+
+    def add_message(self, msg: messages.BECMessage, endpoint: str, public: str = None):
+        self._send_buffer.put((msg, endpoint, public))
+
+    def _buffered_publish(self):
+        try:
+            while not self._buffered_publisher_stop_event.is_set():
+                self._publish_data()
+        except Exception as e:
+            logger.error(f"Error in buffered publisher: {e}")
+            raise
+
+    def _get_messages_from_buffer(self) -> list:
+        msgs_to_send = []
+        while not self._send_buffer.empty():
+            msgs_to_send.append(self._send_buffer.get())
+        return msgs_to_send
+
+    def _publish_data(self) -> None:
+        msgs_to_send = self._get_messages_from_buffer()
+
+        if not msgs_to_send:
+            time.sleep(0.1)
+            return
+
+        pipe = self.connector.pipeline()
+        msgs = messages.BundleMessage()
+        _, endpoint, _ = msgs_to_send[0]
+        for msg, endpoint, public in msgs_to_send:
+            msg_dump = msg
+            msgs.append(msg_dump)
+            if public:
+                self.connector.set(public, msg_dump, pipe=pipe, expire=1800)
+        self.connector.send(endpoint, msgs, pipe=pipe)
+        pipe.execute()
 
     def on_scan_point_emit(self, scan_id: str, point_id: int):
         self._send_bec_scan_point(scan_id, point_id)
@@ -80,3 +128,9 @@ class BECEmitter(EmitterBase):
             return
         num_points = status_msg.info.get("num_points", 0) - 1
         self._update_scan_progress(status_msg.scan_id, num_points, done=True)
+
+    def shutdown(self):
+        if self._buffered_connector_thread:
+            self._buffered_publisher_stop_event.set()
+            self._buffered_connector_thread.join()
+            self._buffered_connector_thread = None
--- bec_server/tests/tests_scan_bundler/test_bec_emitter.py
+++ bec_server/tests/tests_scan_bundler/test_bec_emitter.py
@@ -9,8 +9,11 @@ from bec_server.scan_bundler.bec_emitter import BECEmitter
 
 @pytest.fixture
 def bec_emitter_mock(scan_bundler_mock):
-    emitter = BECEmitter(scan_bundler_mock)
-    yield emitter
+    with mock.patch.object(BECEmitter, "_start_buffered_connector") as start:
+        emitter = BECEmitter(scan_bundler_mock)
+        start.assert_called_once()
+        with mock.patch.object(emitter, "_get_messages_from_buffer"):
+            yield emitter
     emitter.shutdown()
 
 
@@ -64,3 +67,95 @@ def test_send_baseline_BEC(bec_emitter_mock):
         connector.set_and_publish.assert_called_once_with(
             MessageEndpoints.scan_baseline(), msg, pipe=pipe
         )
+
+
+@pytest.mark.parametrize(
+    "msgs",
+    [
+        ([]),
+        (
+            [
+                (
+                    messages.ScanMessage(point_id=1, scan_id="scan_id", data={}, metadata={}),
+                    "endpoint",
+                    None,
+                )
+            ]
+        ),
+        (
+            [
+                (
+                    messages.ScanMessage(point_id=1, scan_id="scan_id", data={}, metadata={}),
+                    "endpoint",
+                    None,
+                ),
+                (
+                    messages.ScanMessage(point_id=2, scan_id="scan_id", data={}, metadata={}),
+                    "endpoint",
+                    None,
+                ),
+            ]
+        ),
+        (
+            [
+                (
+                    messages.ScanMessage(point_id=1, scan_id="scan_id", data={}, metadata={}),
+                    "endpoint",
+                    "public_endpoint",
+                ),
+                (
+                    messages.ScanMessage(point_id=2, scan_id="scan_id", data={}, metadata={}),
+                    "endpoint",
+                    "public_endpoint",
+                ),
+            ]
+        ),
+    ],
+)
+def test_publish_data(msgs, bec_emitter_mock):
+    connector = bec_emitter_mock.connector = mock.MagicMock()
+    bec_emitter_mock._get_messages_from_buffer.return_value = msgs
+    bec_emitter_mock._publish_data()
+    bec_emitter_mock._get_messages_from_buffer.assert_called_once()
+
+    if not msgs:
+        connector.send.assert_not_called()
+        return
+
+    pipe = connector.pipeline()
+    msgs_bundle = messages.BundleMessage()
+    _, endpoint, _ = msgs[0]
+    for msg, endpoint, public in msgs:
+        msg_dump = msg
+        msgs_bundle.append(msg_dump)
+        if public:
+            connector.set.assert_has_calls(connector.set(public, msg_dump, pipe=pipe, expire=1800))
+
+    connector.send.assert_called_with(endpoint, msgs_bundle, pipe=pipe)
+
+
+@pytest.mark.parametrize(
+    "msg,endpoint,public",
+    [
+        (
+            messages.ScanMessage(point_id=1, scan_id="scan_id", data={}, metadata={}),
+            "endpoint",
+            None,
+        ),
+        (
+            messages.ScanMessage(point_id=1, scan_id="scan_id", data={}, metadata={}),
+            "endpoint",
+            "public",
+        ),
+    ],
+)
+def test_add_message(msg, endpoint, public):
+    connector = mock.MagicMock()
+    emitter = BECEmitter(connector)
+    emitter.add_message(msg, endpoint, public)
+    msgs = emitter._get_messages_from_buffer()
+    out_msg, out_endpoint, out_public = msgs[0]
+    assert out_msg == msg
+    assert out_endpoint == endpoint
+    assert out_public == public
+    emitter.shutdown()
